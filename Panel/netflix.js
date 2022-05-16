19c53966deedd0c02071921eb1ab6cd66f871957
foloooxx <97497461+foloooxx@users.noreply.github.com>
2022-05-16 15:45:01 +0800

    Update netflix.js

--- Panel/netflix.js
+++ Panel/netflix.js
@@ -31,7 +31,7 @@ const FILM_ID = 81215567
 const AREA_TEST_FILM_ID = 80018499
 const DEFAULT_OPTIONS = {
   title: '𝐍𝐄𝐓𝐅𝐋𝐈𝐗',
-  fullContent: 'UNLOCK  ➟ #REGION_FLAG# #REGION_CODE#',
+  fullContent: 'Unlock  ➟ #REGION_FLAG# #REGION_CODE#',
   fullIcon: 'bonjour',
   fullIconColor: '#FB8C00',
   fullStyle: '',
