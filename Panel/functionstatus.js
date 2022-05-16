05bb3199bbd45a4aefa95bc90de1b5ccd522dd73
foloooxx <97497461+foloooxx@users.noreply.github.com>
2022-05-16 22:19:01 +0800

    Update functionstatus.js

--- Panel/functionstatus.js
+++ Panel/functionstatus.js
@@ -19,8 +19,8 @@ $done({
 //title:"𝐒𝐔𝐑𝐆𝐄®:  "+startTime
     title:"𝐒𝐔𝐑𝐆𝐄®",
     content:`Running:  ${startTime}\n` + "Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
-    icon: icon_s?"point.3.filled.connected.trianglepath.dotted":"point.3.connected.trianglepath.dotted",
-   "icon-color":icon_s?"#F06292":"#FBC02D"
+    icon: icon_s?"gear.circle":"gear.circle.fill",
+   "icon-color":icon_s?"#CA8D4E":"#FBC02D"
 });
 })();
 function icon_status(status){
