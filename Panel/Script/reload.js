1b2ebfd0c937dd87ccef96b64670420553d78b0b
foloooxx <97497461+foloooxx@users.noreply.github.com>
2022-05-16 09:55:06 +0800

    Delete Panel/Script directory

--- Panel/Script/reload.js
+++
@@ -1,9 +0,0 @@
-$httpAPI("POST", "/v1/profiles/reload", {}, data => {
-    $notification.post("配置重载","配置重载成功","")
-    $done({
-        title: "配置重载",
-        content: "配置重载成功",
-        icon: "pencil.and.outline",
-        "icon-color": "#FF9500",
-     })
-    });
