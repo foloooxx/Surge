f766d0aacb012e17c56313d59304e9ab04f4aa46
foloooxx <97497461+foloooxx@users.noreply.github.com>
2022-05-16 14:35:59 +0800

    Update netinfo.js

--- Panel/netinfo.js
+++ Panel/netinfo.js
@@ -29,7 +29,8 @@ const v4IP = v4.primaryAddress;
         content: `IP：${ip} \n`
             + (wifi.ssid ? `Router：${router}\n` : "")
             + `External：${externalIP}`,
-        icon: wifi.ssid ? "wifi" : "antenna.radiowaves.left.and.right"
+        icon: wifi.ssid ? "flame" : "drop"
+        "icon-color": wifi.ssid ? "#E53935" : "#039BE5"
     };
     $.done(body);
 })();
