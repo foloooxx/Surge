be8feb862be3b00e4a3b1958019452da36fad836
foloooxx <97497461+foloooxx@users.noreply.github.com>
2022-05-16 14:12:50 +0800

    Update functionstatus.js

--- Panel/functionstatus.js
+++ Panel/functionstatus.js
@@ -16,8 +16,9 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"𝐒𝐔𝐑𝐆𝐄®"+startTime,
-    content:`已运行: ${startTime}\n` + "Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
+//title:"𝐒𝐔𝐑𝐆𝐄®:  "+startTime
+    title:"𝐒𝐔𝐑𝐆𝐄®",
+    content:`Running:  ${startTime}\n` + "Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"gear.circle":"gear.circle",
    "icon-color":icon_s?"#1d2ea3":"#dc143c"
 });
